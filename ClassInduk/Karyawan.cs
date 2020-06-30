b39d3e2792f302b87956471fcb84076e255c602f
HerjunoNugroho <61776693+HerjunoNugroho@users.noreply.github.com>
2020-06-30 20:25:50 +0700

    Tugas Lab 9 (Pertemuan 12)

---
+++ ClassInduk/Karyawan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InPolyAbsColl
+{
+    public abstract class Karyawan
+    {
+        public string Nama { get; set; }
+        public string Nik { get; set; }
+        public abstract double Gaji();
+    }
+}
